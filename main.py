81a05167fcd34ad5a172769a772ec77df9260e9a
ilya-shevtsov <Dayzilya@gmail.com>
2024-04-18 03:18:46 +0400

    created title, label and a text box

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 
-
 root = tk.Tk()
+
+root.geometry("500x500")  # window size
+root.title("My First GUI")  # title setting
+
+# create a label
+label = tk.Label(master=root, text="Hello World!", font=('Arial', 18))
+
+# set padding
+label.pack(padx=20, pady=20)
+
+# text box that you can write in
+textBox = tk.Text(root, height=3, font=('Arial', 16))
+textBox.pack()
 
 root.mainloop()
